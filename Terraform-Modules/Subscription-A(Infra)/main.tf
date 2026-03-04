cba54dad51b6354e93c91f60eb34d6f5550ff134
maniknt87 <vroy1187@gmail.com>
2026-03-04 16:30:18 +0530

    terraform and ansible modified

--- Terraform-Modules/Subscription-A(Infra)/main.tf
+++ Terraform-Modules/Subscription-A(Infra)/main.tf
@@ -1,72 +1,142 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "infra-rg"
-  location = var.location
+  name     = "devops-rg"
+  location = "East US"
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "infra-vnet"
+  name                = "devops-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = var.location
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_subnet" "private" {
-  name                 = "private-subnet"
+resource "azurerm_subnet" "subnet" {
+  name                 = "devops-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+# -----------------------------
+# Network Security Group
+# -----------------------------
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "devops-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_security_rule" "ssh" {
+  name                        = "SSH"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_network_security_rule" "rdp" {
+  name                        = "RDP"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_network_security_rule" "http" {
+  name                        = "HTTP"
+  priority                    = 120
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+# -----------------------------
+# Public IPs
+# -----------------------------
+
+resource "azurerm_public_ip" "linux_pip" {
+  name                = "linux-vm-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_public_ip" "windows_pip" {
+  name                = "windows-vm-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+# -----------------------------
+# Network Interfaces
+# -----------------------------
+
 resource "azurerm_network_interface" "linux_nic" {
-  count               = 2
-  name                = "linux-nic-${count.index}"
-  location            = var.location
+  name                = "linux-nic"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.private.id
+    name                          = "linux-ipconfig"
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.linux_pip.id
   }
 }
 
 resource "azurerm_network_interface" "windows_nic" {
   name                = "windows-nic"
-  location            = var.location
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.private.id
+    name                          = "windows-ipconfig"
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.windows_pip.id
   }
 }
 
-resource "azurerm_subnet" "bastion" {
-  name                 = "AzureBastionSubnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
+# -----------------------------
+# Linux VM
+# -----------------------------
+
 resource "azurerm_linux_virtual_machine" "linux_vm" {
-  count               = 1
-  name                = "linux-vm-${count.index}"
+  name                = "linux-vm"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-  size                = "Standard_D2s_v3"
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B2s"
 
-  admin_username = var.admin_username
-
-  disable_password_authentication = true
+  admin_username = "azureuser"
 
   network_interface_ids = [
-    azurerm_network_interface.linux_nic[count.index].id
+    azurerm_network_interface.linux_nic.id
   ]
 
   admin_ssh_key {
-  username   = var.admin_username
-  public_key = file("/var/lib/jenkins/.ssh/id_rsa.pub")
-}
+    username   = "azureuser"
+    public_key = file("/var/lib/jenkins/.ssh/id_rsa.pub")
+  }
 
   os_disk {
     caching              = "ReadWrite"
@@ -80,15 +150,19 @@ resource "azurerm_linux_virtual_machine" "linux_vm" {
     version   = "latest"
   }
 }
-  
+
+# -----------------------------
+# Windows VM
+# -----------------------------
 
 resource "azurerm_windows_virtual_machine" "windows_vm" {
   name                = "windows-vm"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-  size                = "Standard_D2s_v3"
-  admin_username      = var.admin_username
-  admin_password      = var.admin_password
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B2s"
+
+  admin_username = "azureuser"
+  admin_password = "Password123!"
 
   network_interface_ids = [
     azurerm_network_interface.windows_nic.id
@@ -102,27 +176,7 @@ resource "azurerm_windows_virtual_machine" "windows_vm" {
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
+    sku       = "2022-datacenter-azure-edition"
     version   = "latest"
-  }
-}
-
-resource "azurerm_public_ip" "bastion_pip" {
-  name                = "bastion-pip"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-resource "azurerm_bastion_host" "bastion" {
-  name                = "infra-bastion"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                 = "configuration"
-    subnet_id            = azurerm_subnet.bastion.id
-    public_ip_address_id = azurerm_public_ip.bastion_pip.id
   }
 }
