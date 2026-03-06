69a92676bcbf15084fdf2ad6ab4ca50ad4f7e7f2
maniknt87 <vroy1187@gmail.com>
2026-03-06 16:08:02 +0530

    terraform and ansible update

--- Terraform-Modules/Subscription-A(Infra)/main.tf
+++ Terraform-Modules/Subscription-A(Infra)/main.tf
@@ -135,7 +135,7 @@ resource "azurerm_linux_virtual_machine" "linux_vm" {
   name                = "linux-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B2s"
+  size                = "Standard_B2as_v2"
 
   admin_username = "azureuser"
 
@@ -169,7 +169,7 @@ resource "azurerm_windows_virtual_machine" "windows_vm" {
   name                = "windows-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B2s"
+  size                = "Standard_B2as_v2"
 
   admin_username = "azureuser"
   admin_password = "Password123!"
