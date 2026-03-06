b1ddf187ea0b7934eac8cb90c3a03e51cc4667f5
maniknt87 <vroy1187@gmail.com>
2026-03-06 19:53:37 +0530

    terraform and ansible update

--- Terraform-Modules/Subscription-A(Infra)/main.tf
+++ Terraform-Modules/Subscription-A(Infra)/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_subnet" "subnet" {
 # -----------------------------
 
 resource "azurerm_network_security_group" "nsg" {
-  name                = "devops-nsg"
+  name                = "infra-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
